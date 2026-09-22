e7ae4585222881e713f30b6940b1aa20c7e384b7
masimbacunningham-ui <masimbacunningham@gmail.com>
2026-09-22 16:53:38 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Hey Jude',
+  description: 'Shared Household Financial Assistant',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0, fontFamily: 'sans-serif' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
